41ab34893e8d12719875c288df0bb6d219c52b14
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-07-24 05:02:09 +0300

    reflection fluent extensions.

---
+++ Common/ReflectionFluentExts.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ReflectionFluentExts
+    {
+        /// <summary>
+        /// Checks if there are any changes between two versions of a model and updates the 
+        /// values of the specified properties in the old/base model with new 
+        /// property values from the updated model.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="baseModel"></param>
+        /// <param name="updatedModel"></param>
+        /// <param name="propertySelectors"></param>
+        /// <returns></returns>
+        public static TModel UpdateWith<TModel>(this TModel baseModel, TModel updatedModel, params Expression<Func<TModel,object>>[] propertySelectors) 
+            where TModel : class
+        {
+            if (baseModel == null)
+                throw new ArgumentNullException(nameof(baseModel));
+
+            if (updatedModel == null)
+                throw new ArgumentNullException(nameof(updatedModel));
+
+            string[] props = baseModel.GetMemberNames(propertySelectors);
+
+            for (int i = 0; i < props.Length; i++)
+            {
+                object oldValue = baseModel.GetPropertyValue<TModel, object>(props[i]);
+                object newValue = updatedModel.GetPropertyValue<TModel, object>(props[i]);
+
+                if (newValue != oldValue)
+                    baseModel.SetPropertyValue(props[i], newValue);
+            }
+
+            return baseModel;
+        }
+        private static string GetMemberName<T, TProperty>(this T instance, Expression<Func<T, TProperty>> expression)
+        {
+            return GetMemberName(expression.Body);
+        }
+        private static string[] GetMemberNames<T, TProperty>(this T instance, params Expression<Func<T, TProperty>>[] expressions)
+        {
+            List<string> memberNames = new List<string>();
+            foreach (var cExpression in expressions)
+                memberNames.Add(GetMemberName(cExpression.Body));
+
+            return memberNames.ToArray();
+        }
+        private static string GetMemberName<T>(this T instance, Expression<Action<T>> expression)
+        {
+            return GetMemberName(expression.Body);
+        }
+        private static string GetMemberName(Expression expression)
+        {
+            if (expression == null)
+                throw new ArgumentException("Expression cannot be null when getting object member name.");
+
+            if (expression is MemberExpression)
+            {
+                // Reference type property or field
+                var memberExpression = (MemberExpression)expression;
+                return memberExpression.Member.Name;
+            }
+
+            if (expression is MethodCallExpression)
+            {
+                // Reference type method
+                var methodCallExpression = (MethodCallExpression)expression;
+                return methodCallExpression.Method.Name;
+            }
+
+            if (expression is UnaryExpression)
+            {
+                // Property, field of method returning value type
+                var unaryExpression = (UnaryExpression)expression;
+                return GetMemberName(unaryExpression);
+            }
+
+            throw new ArgumentException("Expression passed to get member name is invalid.");
+        }
+        private static string GetMemberName(UnaryExpression unaryExpression)
+        {
+            if (unaryExpression.Operand is MethodCallExpression)
+            {
+                var methodExpression = (MethodCallExpression)unaryExpression.Operand;
+                return methodExpression.Method.Name;
+            }
+
+            return ((MemberExpression)unaryExpression.Operand).Member.Name;
+        }
+    }
+}
---
+++ TestCases/Common.UnitTests/ReflectionFluentExtsTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+
+namespace Common.UnitTests
+{
+    public class ReflectionFluentExtsTests
+    {
+        [Test]
+        public void UpdateOldModel_With_UpdatedModel()
+        {
+            // Arrange
+            Car old = new Car("Bmw","Black");
+            Car newCar = new Car("Audi","White");
+
+            // Act
+            old = old.UpdateWith(newCar, new Expression<Func<Car, object>>[] 
+            { x => x.Name, x => x.Color });
+
+            // Assert
+            Assert.IsNotNull(old);
+            Assert.AreEqual(newCar.Name, old.Name);
+            Assert.AreEqual(10, newCar.Id);
+            Assert.AreEqual(10, old.Id);
+            Assert.AreEqual(newCar.Color, old.Color);
+        }
+    }
+    public class Car
+    {
+        public int Id { get; set; } = 10;
+        public Car(string name, string color="black")
+        {
+            Name = name;
+            Color = color;
+        }
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}
