1041906b79492a6fe71de604b154072ab21fe5ab
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-07-26 19:33:11 +0300

    adds method & tests for getting property changes between two models.

--- Common/ReflectionFluentExts.cs
+++ Common/ReflectionFluentExts.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using Common.Models;
 
 namespace Common
 {
@@ -40,6 +41,37 @@ namespace Common
             }
 
             return baseModel;
+        }
+        /// <summary>
+        /// Checks if there are any changes between two versions of a model and updates the 
+        /// values of the specified properties in the old/base model with new 
+        /// property values from the updated model.
+        /// </summary>
+        /// <typeparam name="TModel"></typeparam>
+        /// <param name="baseModel"></param>
+        /// <param name="updatedModel"></param>
+        /// <param name="propertySelectors"></param>
+        /// <returns></returns>
+        public static UpdateResult<TModel> GetPropertyUpdates<TModel>(this TModel baseModel, TModel updatedModel, params Expression<Func<TModel, object>>[] propertySelectors)
+            where TModel : class
+        {
+            UpdateResult<TModel> updates = new UpdateResult<TModel>
+            {
+                BaseModel = baseModel ?? throw new ArgumentNullException(nameof(baseModel)),
+                UpdatedModel = updatedModel ?? throw new ArgumentNullException(nameof(updatedModel))
+            };
+
+            string[] props = baseModel.GetMemberNames(propertySelectors);
+
+            for (int i = 0; i < props.Length; i++)
+            {
+                object oldValue = baseModel.GetPropertyValue<TModel, object>(props[i]);
+                object newValue = updatedModel.GetPropertyValue<TModel, object>(props[i]);
+
+                updates.PropertyUpdates.Add(new PropertyUpdateResult(props[i],oldValue,newValue));
+            }
+
+            return updates;
         }
         private static string GetMemberName<T, TProperty>(this T instance, Expression<Func<T, TProperty>> expression)
         {
