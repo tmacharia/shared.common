16a17b7752a5ed79be98c13b3514b8e9a3e8511f
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-07-26 19:32:06 +0300

    updates data formatting extension methods

---
+++ Common/Enums/DataFormat.cs
@@ -0,0 +1,29 @@
+﻿namespace Common.Enums
+{
+    /// <summary>
+    /// Represents data formats.
+    /// </summary>
+    public enum DataFormat
+    {
+        /// <summary>
+        /// Terabyte
+        /// </summary>
+        TB,
+        /// <summary>
+        /// Gigabyte
+        /// </summary>
+        GB,
+        /// <summary>
+        /// Megabyte
+        /// </summary>
+        MB,
+        /// <summary>
+        /// Kilobyte
+        /// </summary>
+        KB,
+        /// <summary>
+        /// Bytes
+        /// </summary>
+        Bytes
+    }
+}
---
+++ Common/Primitives/DataSizeExts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Common.Enums;
+
+namespace Common.Primitives
+{
+    /// <summary>
+    /// Represents extension methods used in formatting data byte(s) to
+    /// human readable format.
+    /// </summary>
+    public static class DataSizeExts
+    {
+        private const long OneKb = 1024;
+        private const long OneMb = OneKb * 1024;
+        private const long OneGb = OneMb * 1024;
+        private const long OneTb = OneGb * 1024;
+        /// <summary>
+        /// Convert the specified data byte(s) to the specified <see cref="DataFormat"/> with 
+        /// <see cref="DataFormat.MB"/> being the default.
+        /// </summary>
+        /// <param name="value">Bytes length/count.</param>
+        /// <param name="format">Data format type to convert to.</param>
+        /// <param name="precision">Number of decimal places.</param>
+        /// <returns>Converted value.</returns>
+        public static double ConvertData(this long value, DataFormat format = DataFormat.MB,
+            int precision = 2)
+        {
+            switch (format)
+            {
+                case DataFormat.TB:
+                    return Math.Round((double)value / OneTb, precision);
+                case DataFormat.GB:
+                    return Math.Round((double)value / OneGb, precision);
+                case DataFormat.MB:
+                    return Math.Round((double)value / OneMb, precision);
+                case DataFormat.KB:
+                    return Math.Round((double)value / OneKb, precision);
+                case DataFormat.Bytes:
+                    return value;
+                default:
+                    return 0;
+            }
+        }
+        /// <summary>
+        /// Formats data byte(s) to human readable format appending the appropriate <see cref="DataFormat"/>
+        /// if not specified.
+        /// </summary>
+        /// <param name="value">Bytes length/count.</param>
+        /// <param name="format">Data format type to convert to.</param>
+        /// <param name="precision">Number of decimal places.</param>
+        /// <returns>Formatted data string.</returns>
+        public static string HumanizeData(this long value, DataFormat? format, int precision = 2)
+        {
+            if (!format.HasValue) {
+                format = value >= OneTb ? DataFormat.TB
+                : value >= OneGb ? DataFormat.GB
+                : value >= OneMb ? DataFormat.MB : DataFormat.KB;
+            }
+            return string.Format("{0} {1}",
+                value.ConvertData(format.Value, precision),
+                format.ToString());
+        }
+    }
+}
