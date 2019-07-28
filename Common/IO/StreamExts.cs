f3ec5261a644daa80cf547990ab463373c26e2d7
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-07-28 21:04:57 +0300

    writes tests for Stream exts

--- Common/IO/StreamExts.cs
+++ Common/IO/StreamExts.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace Common.IO
 {
@@ -11,15 +12,18 @@ namespace Common.IO
         /// Converts any object that inherits from <see cref="Stream"/> to a 
         /// byte[]
         /// </summary>
-        /// <param name="input">Input stream.</param>
+        /// <param name="inputStream">Input stream.</param>
         /// <returns>Byte Array</returns>
-        public static byte[] ToArray(this Stream input)
+        public static byte[] ToArray(this Stream inputStream)
         {
+            if (inputStream == null)
+                throw new ArgumentNullException(nameof(inputStream));
+
             byte[] buffer = new byte[16 * 1024];
             using (MemoryStream ms = new MemoryStream())
             {
                 int read;
-                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                while ((read = inputStream.Read(buffer, 0, buffer.Length)) > 0)
                 {
                     ms.Write(buffer, 0, read);
                 }
---
+++ TestCases/Common.UnitTests/StreamExtsTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Common.IO;
+using NUnit.Framework;
+
+namespace Common.UnitTests
+{
+    public class StreamExtsTests
+    {
+        [Test]
+        public void ConvertStreamToArray()
+        {
+            // Arrange
+            Stream stream = new MemoryStream(new byte[1]);
+            // Act
+            byte[] bytes = stream.ToArray();
+            // Assert
+            Assert.IsNotNull(bytes);
+            Assert.IsTrue(bytes.Length == stream.Length);
+        }
+        [Test]
+        public void ConvertStreamToArray_If_StreamIsNull_ThrowsException()
+        {
+            // Arrange; 
+            Stream stream = null;
+            // Act; Assert
+            Assert.Throws<ArgumentNullException>(() => stream.ToArray());
+        }
+    }
+}
