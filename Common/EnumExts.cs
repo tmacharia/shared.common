cc52514f044457d58e674956df04834a8ffba231
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-08-01 18:38:33 +0300

    adds enum get name & writes its tests

---
+++ Common/EnumExts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Common
+{
+    /// <summary>
+    /// Represents extension methods on Enum objects
+    /// </summary>
+    public static class EnumExts
+    {
+        /// <summary>
+        /// Returns the name of current/selected enum
+        /// </summary>
+        /// <typeparam name="TEnum"></typeparam>
+        /// <param name="enum">Current/Selected enum</param>
+        /// <returns>Name of enum</returns>
+        public static string GetName<TEnum>(this TEnum @enum) where TEnum : Enum
+        {
+            if (@enum == null)
+                throw new ArgumentNullException(nameof(@enum));
+            
+            return Enum.GetName(typeof(TEnum), @enum);
+        }
+    }
+}
---
+++ TestCases/Common.UnitTests/EnumExtsTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Common.Enums;
+using NUnit.Framework;
+
+namespace Common.UnitTests
+{
+    public class EnumExtsTests
+    {
+        [Test]
+        public void GetNameOf_SelectedEnum()
+        {
+            // Arrange
+            DataFormat format = DataFormat.Bytes;
+
+            // Act
+            string name = format.GetName();
+
+            // Assert
+            Assert.IsNotNull(name);
+            Console.WriteLine(name);
+        }
+    }
+}
