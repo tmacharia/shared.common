7340b7f1ed00922b57bc9d5dd2ffe622152e8656
tmacharia <29064426+tmacharia@users.noreply.github.com>
2019-08-13 19:14:18 +0300

    adds more tests for string exts

--- TestCases/Common.UnitTests/StringExtsTests.cs
+++ TestCases/Common.UnitTests/StringExtsTests.cs
@@ -2,7 +2,7 @@
 
 namespace Common.UnitTests
 {
-    public class StringExtsTests
+    public class StringExtsTests : TestData
     {
         [Theory]
         #region Valid Email Test Cases
@@ -33,6 +33,83 @@ namespace Common.UnitTests
 
             // Assert
             Assert.AreEqual(isEmailValid, result);
+        }
+
+        [Theory]
+        [TestCase(arg1: "", arg2: 5)]
+        [TestCase(arg1: " ", arg2: 5)]
+        [TestCase(arg1: "Message is a test", arg2: 5)]
+        [TestCase(arg1: "This is a test message", arg2: 25)]
+        public void TruncateText_By_CharacterCount(string text, int count)
+        {
+            // Arrange
+            string trail = Constants.TrailingText;
+            // Act
+            string res = text.Shorten(count);
+
+            // Assert
+            Log(res);
+            if (text.Length > count)
+            {
+                Assert.AreEqual(count + trail.Length, res.Length);
+                Assert.IsTrue(res.EndsWith(trail));
+            }
+            else
+            {
+                Assert.AreEqual(text, res);
+                Assert.IsFalse(res.EndsWith(trail));
+            }
+        }
+        [Theory]
+        [TestCase(arg1: "", arg2: 5)]
+        [TestCase(arg1: " ", arg2: 5)]
+        [TestCase(arg1: "Message is a test", arg2: 2)]
+        [TestCase(arg1: "This is a test message ", arg2: 25)]
+        [TestCase(arg1: " Asp.Net Core 2.1 is awesome", arg2: 3)]
+        public void TruncateText_By_WordCount(string text, int count)
+        {
+            // Arrange
+            string trail = Constants.TrailingText;
+            int wordCount = text.WordCount();
+            // Act
+            string res = text.TruncateWords(count);
+
+            // Assert
+            Log(res);
+            if (wordCount > count)
+            {
+                Assert.AreEqual(count, res.WordCount());
+                Assert.IsTrue(res.EndsWith(trail));
+            }
+            else
+            {
+                Assert.AreEqual(text, res);
+            }
+        }
+
+        [Theory]
+        [TestCase("")]
+        [TestCase(" ")]
+        [TestCase("https://ionicons.com/")]
+        [TestCase("https://radioapp.tk/stations/1/classic-105")]
+        [TestCase("'Cyrillic' is not a supported encoding name.")]
+        public void Url_Slugify(string txt)
+        {
+            // Arrange
+            bool isSlug = txt.IsValidUrlSlug();
+            // Act
+            string slug = txt.GenerateUrlSlug();
+            // Assert
+            Log(slug);
+            if (txt.IsValid())
+            {
+                Assert.IsTrue(slug.IsValidUrlSlug());
+                Assert.IsFalse(isSlug);
+            }
+            else
+            {
+                Assert.AreEqual(txt, slug);
+            }
         }
     }
 }
